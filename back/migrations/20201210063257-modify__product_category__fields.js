bc35b900c6352434349b3bc96cc2e5e8b19cc732
markYoon <markup3604@gmail.com>
2020-12-10 15:41:36 +0900

    fix: migration table

---
+++ back/migrations/20201210063257-modify__product_category__fields.js
@@ -0,0 +1,21 @@
+'use strict';
+const { enum_product_type } = require("../constants/model_enum");
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.changeColumn('Product_category','category', {
+        type: DataTypes.ENUM(enum_product_type),
+        allowNull: false,
+      })
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.changeColumn('Product_category','category', {
+        type: DataTypes.ENUM(enum_product_type),
+        allowNull: false,
+      })
+    ]);
+  }
+};

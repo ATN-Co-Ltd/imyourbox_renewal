a7ab5b3e0fa5f50f5b0c8eeaae013e6eb795f0f7
markYoon <markup3604@gmail.com>
2021-02-04 15:18:51 +0900

    add: google analytics

--- front_renew/src/js/lib/marketing/google.js
+++ front_renew/src/js/lib/marketing/google.js
@@ -13,6 +13,16 @@ googleScriptone.async = true;
 googleScriptone.src= 'https://www.googletagmanager.com/gtag/js?id=AW-611833307';
 document.getElementsByTagName("script")[0].parentNode.appendChild(googleScriptone);
 
+const googleScriptoneFun = () => {
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-611833307");
+}
+
 
 
 // <!-- Global site tag (gtag.js) - Google Ads: 611833307 -->
@@ -22,8 +32,16 @@ googleScripttwo.src= 'https://www.googletagmanager.com/gtag/js?id=UA-146580343-1
 document.getElementsByTagName("script")[0].parentNode.appendChild(googleScripttwo);
 
 
+const googleScriptTwoFun = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "UA-146580343-1");
+}
 
 
-gtag("js", new Date());
-gtag("config", "UA-146580343-1");
-gtag("config", "AW-611833307");
+// gtag("js", new Date());
+// gtag("config", "UA-146580343-1");
+// gtag("config", "AW-611833307");

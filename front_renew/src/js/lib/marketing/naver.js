e31f44bfa3c5376651a29c1e59af215ab8843ff9
markYoon <markup3604@gmail.com>
2021-02-19 15:50:14 +0900

    fix: naver

--- front_renew/src/js/lib/marketing/naver.js
+++ front_renew/src/js/lib/marketing/naver.js
@@ -6,29 +6,15 @@ export function naverConv(){
         var _nasa={};
         _nasa["cnv"] = wcs.cnv("4","1"); // 전환유형, 전환가치 설정해야함. 설치매뉴얼 참고
         wcs_do(_nasa);
+        console.log('naver?');
     })
     naverScript.type='text/javascript';
     naverScript.src="//wcs.naver.net/wcslog.js";
     naverScript.async = true;
     document.getElementsByTagName("script")[0].parentNode.appendChild(naverScript);
-}
+};
 
 
-const naverCommonScript = document.createElement('script');
-naverCommonScript.addEventListener('load',()=> {
-  
-    if (!wcs_add) var wcs_add={};
-    wcs_add["wa"] = "s_1083285baf10";
-    if (!_nasa) var _nasa={};
-    if (window.wcs) {
-     wcs.inflow("imyourbox.com");
-     wcs_do(_nasa);
-    }
-});
-naverCommonScript.type='text/javascript';
-naverCommonScript.defer= true;
-naverCommonScript.src="//wcs.naver.net/wcslog.js";
 
-document.getElementsByTagName("script")[0].parentNode.appendChild(naverCommonScript);
 
 

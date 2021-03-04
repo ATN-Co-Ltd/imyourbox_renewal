3a6578d8abea380f8c14330ea24256a0e37b2738
markYoon <markup3604@gmail.com>
2021-03-04 09:48:42 +0900

    fix:mobion

--- front_renew/src/js/lib/marketing/mob.js
+++ front_renew/src/js/lib/marketing/mob.js
@@ -1,45 +1,68 @@
 //전환데이터 간단견적
 export const mobConvScriptSimpleOrder =() => {
-    let mobSimpleOrderScript = document.createElement('script');
-    mobSimpleOrderScript.addEventListener('load',()=> {
-        let simpleCompanyName = document.querySelector(".simple_customer_company").value;
-        var cn = new EN();
-        cn.setData("uid", "imyourbox");
-        cn.setData("ordcode", "");
-        cn.setData("qty", "1");
-        cn.setData("price", "1");
-        cn.setData("pnm", simpleCompanyName);
-        cn.setSSL(true);
-        cn.sendConv();
-    })
-    mobSimpleOrderScript.src =
-      "https://cdn.megadata.co.kr/js/en_script/3.6/enliple_min3.6.js";
-    mobSimpleOrderScript.async = true;
-    document
-      .getElementsByTagName("script")[0]
-      .parentNode.appendChild(mobSimpleOrderScript);
+  let simpleCompanyName = document.querySelector(".simple_customer_company").value;
+  
+  (function(a,g,e,n,t){
+    a.enp=a.enp||function(){(a.enp.q=a.enp.q||[]).push(arguments)};
+    n=g.createElement(e);
+    n.defer=!0;
+    n.src="https://cdn.megadata.co.kr/dist/prod/enp_tracker_self_hosted.min.js";
+    t=g.getElementsByTagName(e)[0];t.parentNode.insertBefore(n,t)}
+    )(window,document,"script");
+    enp('create', 'conversion', 'imyourbox', { device: 'w', btnSelector: '.callSimpleOrderBtn', convType: 'etc', productName: simpleCompanyName });
+
+  //let mobSimpleOrderScript = document.createElement('script');
+  // mobSimpleOrderScript.addEventListener('load',()=> {
+  //     var cn = new EN();
+  //     cn.setData("uid", "imyourbox");
+  //     cn.setData("ordcode", "");
+  //     cn.setData("qty", "1");
+  //     cn.setData("price", "1");
+  //     cn.setData("pnm", simpleCompanyName);
+  //     cn.setSSL(true);
+  //     cn.sendConv();
+  // })
+  // mobSimpleOrderScript.src =
+  //   "https://cdn.megadata.co.kr/js/en_script/3.6/enliple_min3.6.js";
+  // mobSimpleOrderScript.async = true;
+  // document
+  //   .getElementsByTagName("script")[0]
+  //   .parentNode.appendChild(mobSimpleOrderScript);
+  //})
 
 }
+
 //전환데이터 상세견적
 export const mobConvScriptDetailOrder =() => {
-  
-    let mobDetailOrderScript = document.createElement('script');
-    mobDetailOrderScript.addEventListener('load',()=> {
-        let detailCompanyName = document.querySelector(".customer_company").value;
-        var cn = new EN();
-        cn.setData("uid", "imyourbox");
-        cn.setData("ordcode", "");
-        cn.setData("qty", "1");
-        cn.setData("price", "1");
-        cn.setData("pnm", detailCompanyName);
-        cn.setSSL(true);
-        cn.sendConv();
-    })
-    mobDetailOrderScript.src =
-      "https://cdn.megadata.co.kr/js/en_script/3.6/enliple_min3.6.js";
-    mobDetailOrderScript.async = true;
-    document
-      .getElementsByTagName("script")[0]
-      .parentNode.appendChild(mobDetailOrderScript);
+  let detailCompanyName = document.querySelector(".customer_company").value;
+  (function(a,g,e,n,t){
+    a.enp=a.enp||function(){(a.enp.q=a.enp.q||[]).push(arguments)};
+    n=g.createElement(e);
+    n.defer=!0;
+    n.src="https://cdn.megadata.co.kr/dist/prod/enp_tracker_self_hosted.min.js";
+    t=g.getElementsByTagName(e)[0];t.parentNode.insertBefore(n,t)}
+    )(window,document,"script");
+    enp('create', 'conversion', 'imyourbox', { device: 'w', btnSelector: '.stepBtn__nextBtn', convType: 'etc', productName: detailCompanyName });
+    
+  // let mobDetailOrderScript = document.createElement('script');
+  // mobDetailOrderScript.addEventListener('load',()=> {
+  //     var cn = new EN();
+  //     cn.setData("uid", "imyourbox");
+  //     cn.setData("ordcode", "");
+  //     cn.setData("qty", "1");
+  //     cn.setData("price", "1");
+  //     cn.setData("pnm", detailCompanyName);
+  //     cn.setSSL(true);
+  //     cn.sendConv();
+  // })
+  // mobDetailOrderScript.src =
+  //   "https://cdn.megadata.co.kr/js/en_script/3.6/enliple_min3.6.js";
+  // mobDetailOrderScript.async = true;
+  // document
+  //   .getElementsByTagName("script")[0]
+  //   .parentNode.appendChild(mobDetailOrderScript);
 
 }
+
+
+
--- front_renew/webpack.config.js
+++ front_renew/webpack.config.js
@@ -85,6 +85,6 @@ module.exports = {
 
   devServer : {
     host:'0.0.0.0',
-    port:3065,
+    port:3000,
   }
 };

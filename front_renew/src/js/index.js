ce000a4de0aa407dd7b7f8acf70d50ea777d958d
markYoon <markup3604@gmail.com>
2021-05-06 17:56:43 +0900

    add: eventPopup

--- front_renew/src/js/index.js
+++ front_renew/src/js/index.js
@@ -1,27 +1,26 @@
-import "core-js/stable"
+import "core-js/stable";
 import "regenerator-runtime/runtime";
-import 'slick-carousel';
-import './lib/marketing/kakao';
-import './lib/marketing/google';
-import './lib/marketing/mirae';
-import './lib/marketing/mob';
-import './lib/marketing/naver';
-import './main';
-import './service';
+import "slick-carousel";
+import "./lib/eventModal";
+import "./lib/marketing/kakao";
+import "./lib/marketing/google";
+import "./lib/marketing/mirae";
+import "./lib/marketing/mob";
+import "./lib/marketing/naver";
+import "./main";
+import "./service";
 import "./access";
-import './introduction';
-import './stepone';
-import '../css/style.css';
-import './stepTwo';
-import './stepThree';
-import './stepFour';
-import './price';
-import './button';
-import './stepresult';
-import KakaoChat from './lib/chat/kakaochat';
+import "./introduction";
+import "./stepone";
+import "../css/style.css";
+import "./stepTwo";
+import "./stepThree";
+import "./stepFour";
+import "./price";
+import "./button";
+import "./stepresult";
+import KakaoChat from "./lib/chat/kakaochat";
 
 KakaoChat();
 
-
-
-console.log('hello webpack!');
+console.log("hello webpack!");

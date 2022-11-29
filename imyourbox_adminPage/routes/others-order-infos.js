93637d6dc26664ba0bfb0b90f0e63a98ca93b425
markYoon <markup3604@gmail.com>
2022-11-29 14:24:24 +0900

    update: price

---
+++ imyourbox_adminPage/routes/others-order-infos.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { othersOrderInfos } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('othersOrderInfos');
+
+// This file contains the logic of every route in Forest Admin for the collection othersOrderInfos:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Others Order Info
+router.post('/othersOrderInfos', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Others Order Info
+router.put('/othersOrderInfos/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Others Order Info
+router.delete('/othersOrderInfos/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Others Order Infos
+router.get('/othersOrderInfos', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Others Order Infos
+router.get('/othersOrderInfos/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#get-a-number-of-records
+  // Improve performances disabling pagination: https://docs.forestadmin.com/documentation/reference-guide/performance#disable-pagination-count
+  next();
+});
+
+// Get a Others Order Info
+router.get('/othersOrderInfos/\\b(?!count\\b):recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Others Order Infos
+router.get('/othersOrderInfos.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of Others Order Infos
+router.delete('/othersOrderInfos', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
